3db8833b4cdcc6eb392a314a9f9b1bff037a4b26
Admin <Admin@TEB127030>
2023-02-15 11:19:57 +0100

    Add project files.

---
+++ upasdfupoasuidfopi/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"6 + 9 + 4 = {Addera(6, 9, 4)}");
+        }
+        static int Addera(int tal1, int tal2, int tal3)
+        {
+            int sum = tal1 + tal2 + tal3;
+            return sum;
+        }
+    }
+}
